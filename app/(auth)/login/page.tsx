fb44e0de14efbed2a24eee89ec308ba25cece207
utso <utsosarkar7870@gmail.com>
2025-04-24 16:05:44 +0600

    login page design complete

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,18 @@
-
+import LoginForm from "@/components/AllForm/LoginForm"
+import loginPage from "@/public/auth/login.jpg"
+import Image from "next/image"
 function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+     <section className='max-w-[1346px] mx-auto min-h-screen flex items-center'>
+      <div className=' grid grid-cols-1 lg:grid-cols-2 gap-8 items-center h-full'>
+           <div className=' rounded-3xl overflow-hidden'>
+              <Image src={loginPage} alt='RegisterImage' width={650} height={750} className=' w-full '/>
+           </div>
+           <div>
+             <LoginForm/>
+           </div>
+      </div>
+    </section>
   )
 }
 
