36f70c8a45c1708bc0570aa538da3cf3ac676ac0
utso <utsosarkar7870@gmail.com>
2025-04-24 12:26:14 +0600

    update

--- nextjs-boilerplate-main/lib/Socket.ts
+++
@@ -1,5 +0,0 @@
-import { AppConfig } from "@/config/app.config";
-import { io } from "socket.io-client";
-
-// initialize socket
-export const socket = io(AppConfig().app.url);
--- nextjs-boilerplate-main/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-    interface Window {
-      katex: typeof import("katex");
-    }
-  }
