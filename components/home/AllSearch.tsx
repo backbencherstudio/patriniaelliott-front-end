41e5a5e885537c64e58cf5d5332da041cb179dfe
utso <utsosarkar7870@gmail.com>
2025-04-27 11:10:05 +0600

    home page all device responsive design complete

--- components/home/AllSearch.tsx
+++ components/home/AllSearch.tsx
@@ -1,16 +1,16 @@
 'use client';
 import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
 } from '@/components/ui/tabs';
 import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import TourSearch from './TourSearch';
 
 export default function AllSearch() {
-  const [tab, setTab] = useState('apartment');
+  const [tab, setTab] = useState('tour');
   return (
     <div className="  rounded-t-md  flex flex-col md:flex-row items-center ">
       {/* Tabs */}
@@ -21,7 +21,7 @@ export default function AllSearch() {
           <TabsTrigger className={`${tab =="hotel" ? "!bg-secondaryColor": "!bg-whiteColor"} data-[state=active]:shadow-none text-base px-9 py-6 rounded-tr-2xl rounded-tl-none rounded-b-none `} value="hotel">Hotel</TabsTrigger>
         </TabsList>
         <TabsContent value='tour'>
-            <div className=' absolute -bottom-24   left-1/2 w-auto  -translate-x-1/2'>
+            <div className=' absolute top-[100%] lg:-bottom-24   left-1/2 w-auto  -translate-x-1/2'>
 
              <TourSearch/>
             </div>
