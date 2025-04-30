a2f7737da2918ac9acd8c0c6f5dc19516fb7cb9f
utso <utsosarkar7870@gmail.com>
2025-04-30 09:08:34 +0600

    build problem solve

--- components/contact/ContactFaq.tsx
+++ components/contact/ContactFaq.tsx
@@ -5,6 +5,9 @@ function ContactFaq() {
   return (
     <div className=' py-14 lg:py-20 '>
        <div className='max-w-5xl mx-auto'>
+        <div>
+           <h2 className=' text-3xl lg:text-5xl font-medium text-headerColor text-center mb-10'>Frequently Asked Questions</h2>
+        </div>
           <Accordion
             type="single"
             collapsible
@@ -18,14 +21,14 @@ function ContactFaq() {
                 className={`px-3  rounded-xl bg-bgColor  border border-[#DADDE3] `}
               >
                 <AccordionTrigger
-                  className={`py-7  cursor-pointer text-left text-base text-grayColor1 font-medium hover:no-underline `}
+                  className={`py-4  cursor-pointer text-left text-base text-grayColor1 font-medium hover:no-underline `}
                 >
                   <h4 className={` text-base lg:text-lg font-medium  text-headerColor`}>
                     {item.question}
                   </h4>
                  
                 </AccordionTrigger>
-                <AccordionContent className="text-base pr-8 text-grayColor1   ">
+                <AccordionContent className="text-base pr-8 text-grayColor1  bg-whiteColor ">
                  <p>{item.answer}</p>
                 </AccordionContent>
               </AccordionItem>
