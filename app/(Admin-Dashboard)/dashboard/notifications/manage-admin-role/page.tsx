e16093d041e17e79b9c88815e087632ca2ec2587
utso <utsosarkar7870@gmail.com>
2025-05-22 11:06:18 +0600

    email notification done

---
+++ app/(Admin-Dashboard)/dashboard/notifications/manage-admin-role/page.tsx
@@ -0,0 +1,19 @@
+import EmailNotificationSettingsForm from '@/app/(Admin-Dashboard)/component/notificationSetting/EmailNotificationSettingsForm'
+import Link from 'next/link'
+import { FaChevronRight } from 'react-icons/fa'
+
+function AdminNotificationPage() {
+  return (
+    <div>
+       <div className=" px-4 mb-20 ">
+      <div className=" flex items-center  md:text-base text-sm gap-2 lg:gap-4 mt-6 lg:mt-0 mb-6">
+
+     <Link href="/dashboard/notifications" className="text-grayColor1">Manage notifications </Link>  <FaChevronRight className="text-grayColor1"/> <p className=" text-headerColor">Manage Admin Role</p>
+      </div>
+      <EmailNotificationSettingsForm/>
+    </div>
+    </div>
+  )
+}
+
+export default AdminNotificationPage
