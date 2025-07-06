f1252f98c6ebe01da56698f991c31147276f93fd
utso <utsosarkar7870@gmail.com>
2025-07-06 14:25:37 +0600

    booking layout done

--- app/(Frond-End)/(my-booking)/component/common/BookingSidebar.tsx
+++ app/(Frond-End)/(my-booking)/component/common/BookingSidebar.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import Image from 'next/image'; 
-import Link from 'next/link'; 
-import { usePathname } from 'next/navigation'; 
-import React from 'react'; 
-import { IoIosArrowForward } from 'react-icons/io'; 
+import { X } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
 
 interface NavItem {
   icon: string;
@@ -25,48 +26,57 @@ const navItems: NavItem[] = [
   { icon: '/booking/refund.svg', label: 'Refund History', href: '/refund-history' },
 ];
 
-const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+const BookingSidbar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
-  
+    const isActive = (href: string): boolean => {
+    if (href === "/profile-info") {
+      return pathname === "/profile-info";
+    }
+    return pathname.startsWith(href);
+  };
   return (
-    <div className="h-screen lg:h-auto">
+     <div className="h-screen lg:h-auto">
       {/* Overlay for mobile */}
       {isOpen && (
         <div 
-          className="fixed inset-0 z-40 md:hidden" 
+          className="absolute top-0 left-0 w-full h-full z-40 xl:hidden" 
           onClick={onClose}
         />
       )}
 
       {/* Sidebar container */}
       <div className={`
-        ${isOpen ? 'inset-0 z-50 h-full overflow-hidden relative' : 'h-full'} 
+        ${isOpen ? 'z-50 !h-full overflow-hidden absolute -top-20 left-0' : 'h-full'} 
         flex flex-col
         min-h-[calc(100vh-100px)] 
         bg-white 
-        border border-[#E2E8F0] shadow-[0px_-0.3px_5.5px_0px_rgba(0,0,0,0.02)] 
-        lg:rounded-[12px] p-5 w-full 
-      `}> 
-        {/* Account Section */} 
-        <div className="mb-4"> 
-          <h2 className="text-sm font-normal text-gray-500 mb-2">Account</h2> 
-          <nav className="flex flex-col gap-1"> 
-            {navItems.slice(0, 5).map((item, idx) => ( 
-              <Link 
-                key={idx} 
-                href={item.href} 
-                onClick={onClose} 
-                className={` 
+        border border-[#E2E8F0] shadow-[0px_-0.3px_5.5px_0px_rgba(0,0,0,0.02)]
+        xl:rounded-[12px] p-5 w-full overflow-y-auto
+      `}>
+        <div className="flex justify-end xl:hidden cursor-pointer">
+          <button onClick={onClose}><X /></button>
+        </div>
+
+        {/* Account Section */}
+        <div className="mb-4">
+          <h2 className="text-sm font-normal text-gray-500 mb-2">Account</h2>
+          <nav className="flex flex-col gap-1">
+            {navItems.slice(0, 5).map((item, idx) => (
+              <Link
+                key={idx}
+                href={item.href}
+                onClick={onClose}
+                className={`
                   flex items-center justify-between gap-3 px-3 py-2.5  rounded-lg 
                   transition-colors duration-200 
-                  ${item.href === pathname ? 'bg-[#FFF7E7]' : 'hover:bg-[#FFF7E7]'} 
-                `} 
-              > 
+                  ${isActive(item.href) ? 'bg-[#FFF7E7]' : 'hover:bg-[#FFF7E7]'}
+                `}
+              >
                 <div className='flex gap-3 items-center'>
- <div className={`w-[30px] flex justify-center items-center   h-[30px] flex-shrink-0 rounded-full ${item.href === pathname ? 'bg-whiteColor' : 'bg-[#FFFBEE]'}  shadow-[0px_-0.3px_5.5px_rgba(0,0,0,0.04)]`}>
+ <div className={`w-[30px] flex justify-center items-center   h-[30px] flex-shrink-0 rounded-full ${isActive(item.href) ? 'bg-whiteColor' : 'bg-[#FFFBEE]'}  shadow-[0px_-0.3px_5.5px_rgba(0,0,0,0.04)]`}>
                   <Image src={item.icon} alt={item.label} width={20} height={20} />
                 </div>
-                <span className="text-base font-normal text-[#111111]">{item.label}</span>
+                <span className="text-base whitespace-nowrap font-normal text-[#111111]">{item.label}</span>
                 </div>
                
                  <span><IoIosArrowForward/></span>
@@ -90,11 +100,11 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
                 className={`
                   flex items-center justify-between gap-3 px-3 py-2.5  rounded-lg 
                   transition-colors duration-200 
-                  ${item.href === pathname ? 'bg-[#FFF7E7]' : 'hover:bg-[#FFF7E7]'}
+                  ${isActive(item.href) ? 'bg-[#FFF7E7]' : 'hover:bg-[#FFF7E7]'}
                 `}
               >
                 <div className='flex gap-3 items-center'>
- <div className={`w-[30px] flex justify-center items-center hover:bg-whiteColor  h-[30px] flex-shrink-0 rounded-full ${item.href === pathname ? 'bg-whiteColor' : 'bg-[#FFFBEE]'}  shadow-[0px_-0.3px_5.5px_rgba(0,0,0,0.04)]`}>
+ <div className={`w-[30px] flex justify-center items-center hover:bg-whiteColor  h-[30px] flex-shrink-0 rounded-full ${isActive(item.href) ? 'bg-whiteColor' : 'bg-[#FFFBEE]'}  shadow-[0px_-0.3px_5.5px_rgba(0,0,0,0.04)]`}>
                   <Image src={item.icon} alt={item.label} width={20} height={20} />
                 </div>
                 <span className="text-base font-normal text-[#111111]">{item.label}</span>
@@ -107,7 +117,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
         </div>
 
         {/* Log out section */}
-        <div className="mt-auto pt-4">
+          <div className="mt-auto pt-4">
           <Link
             href="/logout"
             onClick={onClose}
@@ -124,4 +134,4 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   );
 };
 
-export default Sidebar;
+export default BookingSidbar;
