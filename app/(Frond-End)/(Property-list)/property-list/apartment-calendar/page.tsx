88a673680c8d6b835bed1c9e52e4232ad4ad2a06
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2025-07-09 12:43:54 +0600

    Calendar page added.

--- app/(Frond-End)/(Property-list)/property-list/apartment-calendar/page.tsx
+++ app/(Frond-End)/(Property-list)/property-list/apartment-calendar/page.tsx
@@ -18,12 +18,12 @@ export default function page() {
     return (
         <div className="flex justify-center items-center w-full bg-[#F6F7F7]">
             <div className="py-15 px-4 max-w-[1320px] w-full space-y-[48px]">
-                <ul className="flex w-full justify-between">
+                <ul className="hidden md:flex w-full justify-between">
                     {
                         header.map(item => (
                             <li className="space-y-4 w-fit" key={item}>
                                 <h3 className={`${item === "Calendar" ? "text-[#070707]" : "text-[#777980]"} text-sm text-center`}>{item}</h3>
-                                <div className="w-[210px] h-[12px] bg-[#D9D9D9] rounded-full relative">
+                                <div className="w-[140px] lg:w-[180px] xl:w-[210px] h-[12px] bg-[#D9D9D9] rounded-full relative">
                                     <div className={`absolute top-0 left-0 h-full w-3/4 bg-[#D6AE29] rounded-full ${item === "Calendar" ? "" : "hidden"}`}></div>
                                 </div>
                             </li>
@@ -32,7 +32,7 @@ export default function page() {
                 </ul>
                 <div className="flex justify-between w-full space-x-3 px-4">
                     <div className="text-[#0068EF] px-6 sm:px-[32px] py-2 sm:py-3 border border-[#0068EF] rounded-[8px] cursor-pointer" onClick={() => router.back()}>Back</div>
-                    <button type="button" className="text-[#fff] px-6 sm:px-[32px] py-2 sm:py-3 border border-[#fff] bg-[#0068EF] rounded-[8px] cursor-pointer" onClick={()=>console.log("Form is submitted...")}>Continue</button>
+                    <button type="button" className="text-[#fff] px-6 sm:px-[32px] py-2 sm:py-3 border border-[#fff] bg-[#0068EF] rounded-[8px] cursor-pointer" onClick={() => console.log("Form is submitted...")}>Continue</button>
                 </div>
             </div>
         </div>
