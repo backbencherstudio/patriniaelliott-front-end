1bf4a5b59151f88a5730207d64c3d380f14ea2fc
shurov <shurov.0.00.0@gmail.com>
2025-07-29 09:58:52 +0600

    Vendor details comes on log now

--- service/user/user.service.ts
+++ service/user/user.service.ts
@@ -249,4 +249,17 @@ newPassword: async (data) => {
 
     return await Fetch.patch(`/user/${id}/password`, data, _config);
   },
+
+  submitVendorVerification: async (data: any, context = null) => {
+    const userToken = CookieHelper.get({ key: "token", context });
+
+    const _config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + userToken,
+      },
+    };
+
+    return await Fetch.post("/vendor-verification", data, _config);
+  },
 };
