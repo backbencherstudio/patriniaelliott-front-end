b5b3216ab0c96eafcb582ef465e4c6f50df57856
utso <utsosarkar7870@gmail.com>
2025-08-28 15:05:45 +0600

    Employe page update done

--- components/card/ApartmentCard.tsx
+++ components/card/ApartmentCard.tsx
@@ -73,7 +73,7 @@ const ApartmentCard = ({ hotel }: any) => {
               </div>
           }
         {breakfast_available && (
-          <span className="absolute top-2 left-2 bg-red-600 text-white text-xs px-2 py-1  rounded-full">
+          <span className="absolute top-2 left-2 bg-red-600 text-white text-xs px-2 py-1 z-10  rounded-full">
             Breakfast Included
           </span>
         )}
