d9a3037a78dab364e771545842a8a0e5529e58ff
shurov <shurov.0.00.0@gmail.com>
2025-10-16 16:33:59 +0600

    Pull from main

--- next.config.ts
+++ next.config.ts
@@ -73,7 +73,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "wives-draw-physician-tied.trycloudflare.com",
+        hostname: "blind-domain-lat-net.trycloudflare.com",
       },
     ],
   },
